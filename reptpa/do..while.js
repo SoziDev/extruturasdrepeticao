b715fb066adcfa6b40fcf3827fd18afff610e9fd
sophia <163858535+SoziDev@users.noreply.github.com>
2024-11-27 16:48:11 -0300

    Add files via upload

---
+++ reptpa/do..while.js
@@ -0,0 +1,10 @@
+let i = 4;
+
+while (i <= 100) {
+  if (i % 4 === 0) {
+    i++; 
+  }
+   console.log(i); 
+  
+  i++;  
+}
---
+++ reptpa/while.js
@@ -0,0 +1,9 @@
+let i = 1;
+
+while (i <= 100) {
+  if (i % 5 === 0) {
+    i++; 
+  }
+   console.log(i); 
+   i++;  
+}
